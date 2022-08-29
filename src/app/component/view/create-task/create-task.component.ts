879389c20ade5dbdd7b344cf9ec7bd20db92a5e4
HagarMohamed <hagarabdo7@gmail.com>
2022-08-29 03:05:42 +0200

    Add Validation

--- src/app/component/view/create-task/create-task.component.ts
+++ src/app/component/view/create-task/create-task.component.ts
@@ -1,43 +1,53 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {NgForm, NgModel} from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { TaskServiceService } from 'src/app/services/task-service.service';
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
-  styleUrls: ['./create-task.component.scss']
+  styleUrls: ['./create-task.component.scss'],
 })
 export class CreateTaskComponent implements OnInit {
-
   file = null;
   success = false;
 
-  constructor(private service: TaskServiceService) { }
-
+  constructor(private service: TaskServiceService) {}
 
   ngOnInit(): void {}
 
-  createTask(titleInput: NgModel, contentarea: NgModel, usernameInput: NgModel){
-    this.service.createTask(titleInput.value, contentarea.value, usernameInput.value, this.file, (responce: any) =>{
-      console.log(responce);
+  createTask(
+    titleInput: NgModel,
+    contentarea: NgModel,
+    usernameInput: NgModel
+  ) {
+    this.service.createTask(
+      titleInput.value,
+      contentarea.value,
+      usernameInput.value,
+      this.file,
+      (responce: any) => {
+        console.log(responce);
 
-      if(responce.hasOwnProperty("success")){
-        
-        this.success = true;
+        if (responce.hasOwnProperty('success')) {
+          this.success = true;
+        }
       }
-
-    })
+    );
   }
 
-  
-
   onChange(event: any) {
+    // if (
+    //   event.target.files[0].type === 'image/jpeg' ||
+    //   event.target.files[0].type === 'image/png' ||
+    //   event.target.files[0].type === 'image/jpg'
+    // ) {
+    //   if (event.target.files[0].size < 2048) {
+    //     /* checking size here - 2MB */
+    //     this.file = event.target.files[0];
+    //   }
+    // }
+
     this.file = event.target.files[0];
+  }
 }
-
-
-}
-
-
-
